89da2d8c1cd56500fe5c1bb4d474d8648afb4cbb
Jimmy Thigpen <jmthigpen@me.com>
2015-02-25 17:25:25 -0500

    done

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -18,7 +18,13 @@ var TodosController = Ember.ArrayController.extend({
 
        // Save the new model
        todo.save();
-     }
+     },
+
+     clearCompleted: function() {
+   var completed = this.filterBy('isCompleted', true);
+   completed.invoke('deleteRecord');
+   completed.invoke('save');
+      }
    },
 
    remaining: function () {
@@ -28,8 +34,26 @@ var TodosController = Ember.ArrayController.extend({
    inflection: function () {
      var remaining = this.get('remaining');
      return remaining === 1 ? 'item' : 'items';
-   }.property('remaining')
-   
+   }.property('remaining'),
+
+    hasCompleted: function() {
+     return this.get('completed') > 0;
+    }.property('completed'),
+
+    completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+
+    allAreDone: function(key, value) {
+      if (value === undefined) {
+        return !!this.get('length') && this.isEvery('isCompleted', true);
+    } else {
+    this.setEach('isCompleted', value);
+    this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted')
+
  });
 
 export default TodosController;
